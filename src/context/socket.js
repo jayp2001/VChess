e474961efa20a507652ee48e1e63f386fb82e440
Juzar <juzarbharmal@Juzars-MacBook-Air.local>
2021-10-11 21:47:03 +0530

    changed socket_url

--- src/context/socket.js
+++ src/context/socket.js
@@ -2,7 +2,8 @@ import socketio from "socket.io-client";
 
 import { createContext } from "react";
 
-const SOCKET_URL = "http://localhost:8000";
+const SOCKET_URL = "https://chess-server-react.herokuapp.com/";
+// const SOCKET_URL = "http://localhost:8000";
 // const SOCKET_URL = "https://chess-server-react.herokuapp.com/";
 
 export const socket = socketio.connect(SOCKET_URL);
